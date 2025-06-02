0c1174e36f041a703291763327b6512b5bd4d05d
aleluigimar <aleluigimar@gmail.com>
2025-06-02 11:18:27 +0200

    Exercises folder reorganization + list1 match_ends exercise

---
+++ google-python-exercises/MieiEsercizi/myList1/main.py
@@ -0,0 +1,10 @@
+import myList1
+
+def main():
+    
+    #Esercizio 1: match_ends
+    print(myList1.match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']))  # Expected: 3
+    
+#LAUNCH MAIN SCRIPT    
+if __name__ == '__main__':
+    main()
