a1977301b3bb80792d1b0fe1f4ca6ab7b6bc96a4
aleluigimar <aleluigimar@gmail.com>
2025-06-02 12:01:27 +0200

    string2, verbing exercise

---
+++ google-python-exercises/MieiEsercizi/myString2/main.py
@@ -0,0 +1,11 @@
+import myString2
+
+def main():
+    
+    #Esercizio 1: verbing
+    print(myString2.verbing('sightsee'))
+    
+    
+#LAUNCH MAIN SCRIPT    
+if __name__ == '__main__':
+    main()
