64dae2de56c9db5b5382f296f1f6255fac51b40c
aleluigimar <aleluigimar@gmail.com>
2025-06-02 16:48:26 +0200

    list2, linear_merge exercise

--- google-python-exercises/MieiEsercizi/myList2/main.py
+++ google-python-exercises/MieiEsercizi/myList2/main.py
@@ -3,7 +3,10 @@ import myList2
 def main():
     
     #Esercizio 1: remove_adjacent
-    print(myList2.remove_adjacent([1,2,2,2,2,2,2,3,4,5,5,5,5,6]))
+    #print(myList2.remove_adjacent([1,2,2,2,2,2,2,3,4,5,5,5,5,6]))
+    
+    #Esercizio 2: linear_merge
+    print(myList2.linear_merge([1,3,5,7,9,11],[2,4,6,8,10,12]))
     
     
 #LAUNCH MAIN SCRIPT    
